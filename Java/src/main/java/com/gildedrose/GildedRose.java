ed57e0697a0458f7226a281328e7896a55128e68
toros <tom.vanrossom@tilroy.com>
2024-12-30 11:13:56 +0100

    002-refactor * refactoring "invert if"

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,7 +1,5 @@
 package com.gildedrose;
 
-import com.gildedrose.core.domain.SpecialNames.*;
-
 import static com.gildedrose.core.domain.SpecialNames.*;
 
 class GildedRose {
@@ -19,14 +17,7 @@ class GildedRose {
     }
 
     private void updateQuality(Item item) {
-        if (!AGED_BRIE.equals(item.name)
-                && !BACKSTAGE_PASSES.equals(item.name)) {
-            if (item.quality > 0) {
-                if (!SULFURAS.equals(item.name)) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
+        if (AGED_BRIE.equals(item.name) || BACKSTAGE_PASSES.equals(item.name)) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
 
@@ -44,6 +35,12 @@ class GildedRose {
                     }
                 }
             }
+        } else {
+            if (item.quality > 0) {
+                if (!SULFURAS.equals(item.name)) {
+                    item.quality = item.quality - 1;
+                }
+            }
         }
 
         if (!SULFURAS.equals(item.name)) {
@@ -51,19 +48,19 @@ class GildedRose {
         }
 
         if (item.sellIn < 0) {
-            if (!AGED_BRIE.equals(item.name)) {
-                if (!BACKSTAGE_PASSES.equals(item.name)) {
+            if (AGED_BRIE.equals(item.name)) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            } else {
+                if (BACKSTAGE_PASSES.equals(item.name)) {
+                    item.quality = 0;
+                } else {
                     if (item.quality > 0) {
                         if (!SULFURAS.equals(item.name)) {
                             item.quality = item.quality - 1;
                         }
                     }
-                } else {
-                    item.quality = item.quality - item.quality;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
                 }
             }
         }
