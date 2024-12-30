0947f4280435505858f5ce2f6c3685fdc114bcdf
toros <tom.vanrossom@tilroy.com>
2024-12-30 16:26:09 +0100

    004-add-unittests * add SelectableByNameTest

---
+++ Java/src/test/java/com/gildedrose/core/usecase/selectable/SelectableByNameTest.java
@@ -0,0 +1,36 @@
+package com.gildedrose.core.usecase.selectable;
+
+import com.gildedrose.Item;
+import org.junit.jupiter.api.Test;
+
+import static com.gildedrose.core.domain.SpecialNames.CONJURED;
+import static com.gildedrose.core.domain.SpecialNames.SULFURAS;
+import static com.gildedrose.core.usecase.selectable.Selectables.byName;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SelectableByNameTest {
+
+    @Test
+    void matchesConfigured() {
+        //given
+        Selectable selectable = byName(SULFURAS);
+        Item item = new Item(SULFURAS.fullName, 5, 10);
+
+        //when
+        boolean actual = selectable.matches(item);
+
+        assertTrue(actual);
+    }
+
+    @Test
+    void matchesFalseWithOther() {
+        //given
+        Selectable selectable = byName(SULFURAS);
+        Item item = new Item(CONJURED.fullName, 5, 10);
+
+        //when
+        boolean actual = selectable.matches(item);
+
+        assertFalse(actual);
+    }
+}
