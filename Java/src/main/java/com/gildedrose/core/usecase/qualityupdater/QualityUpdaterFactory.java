96a63ca1cf64c4b12d278ed0470d40111d3fab12
toros <tom.vanrossom@tilroy.com>
2024-12-30 16:22:15 +0100

    004-add-unittests * add LegendaryQualityUpdaterTest

--- Java/src/main/java/com/gildedrose/core/usecase/qualityupdater/QualityUpdaterFactory.java
+++ Java/src/main/java/com/gildedrose/core/usecase/qualityupdater/QualityUpdaterFactory.java
@@ -2,24 +2,23 @@ package com.gildedrose.core.usecase.qualityupdater;
 
 import com.gildedrose.core.usecase.aging.AgingFunctions;
 
-import java.util.Arrays;
-
 import static com.gildedrose.core.domain.SpecialNames.*;
 import static com.gildedrose.core.usecase.aging.AgingFunctions.backstagePasses;
 import static com.gildedrose.core.usecase.aging.AgingFunctions.singleDegrading;
 import static com.gildedrose.core.usecase.selectable.Selectables.all;
 import static com.gildedrose.core.usecase.selectable.Selectables.byName;
+import static java.util.Arrays.asList;
 
 public class QualityUpdaterFactory {
 
     private static QualityUpdater standard = new AgingQualityUpdater(all(), singleDegrading());
     private static SelectableQualityUpdater backStagePassesQualityUpdater = new AgingQualityUpdater(byName(BACKSTAGE_PASSES), backstagePasses());
-    private static SelectableQualityUpdater legendary = new LegendaryQualityUpdater();
+    private static SelectableQualityUpdater legendary = new LegendaryQualityUpdater(byName(SULFURAS));
     private static SelectableQualityUpdater improving = new AgingQualityUpdater(byName(AGED_BRIE), AgingFunctions.improving());
 
     private static SelectableQualityUpdater doubleDegrading = new AgingQualityUpdater(byName(CONJURED), AgingFunctions.doubleDegrading());
 
     public static QualityUpdater create(){
-        return new QualityUpdaterSelector(standard, Arrays.asList(improving, legendary, backStagePassesQualityUpdater, doubleDegrading));
+        return new QualityUpdaterSelector(standard, asList(improving, legendary, backStagePassesQualityUpdater, doubleDegrading));
     }
 }
---
+++ Java/src/test/java/com/gildedrose/core/usecase/qualityupdater/LegendaryQualityUpdaterTest.java
@@ -0,0 +1,39 @@
+package com.gildedrose.core.usecase.qualityupdater;
+
+import com.gildedrose.Item;
+import com.gildedrose.core.domain.SpecialNames;
+import org.junit.jupiter.api.Test;
+
+import static com.gildedrose.core.domain.SpecialNames.SULFURAS;
+import static com.gildedrose.core.usecase.selectable.Selectables.byName;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LegendaryQualityUpdaterTest {
+
+    private final LegendaryQualityUpdater updater = new LegendaryQualityUpdater(byName(SULFURAS));
+
+    @Test
+    void updateQualityNeverChanges() {
+        //given
+        Item item = new Item(SULFURAS.fullName, 5, 50);
+
+        //when
+        updater.updateQuality(item);
+
+        //then
+        assertEquals(80, item.quality);
+        assertEquals(5, item.sellIn);
+    }
+
+    @Test
+    void matchesSulfuras(){
+        //given
+        Item item = new Item(SULFURAS.fullName, 5, 50);
+
+        //when
+        boolean actual = updater.matches(item);
+
+        //then
+        assertTrue(actual);
+    }
+}
